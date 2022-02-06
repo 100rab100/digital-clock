2cfffa8bf74e27d7040d247b4ccd3ebb404ea96b
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-02-06 23:22:52 +0530

    Add files via upload

---
+++ digital clock.py
@@ -0,0 +1,18 @@
+import time
+from tkinter import *
+root=Tk()
+root.geometry("356x150+0+0")
+root.configure(background="black")
+root.resizable(0,0)
+root.overrideredirect(1)
+def start():
+    text=time.strftime("%H:%M:%S")
+    label.config(text=text)
+    label.after(200,start)
+
+
+label=Label(root,font=("ds-digital",50,'bold'),bg='black',fg='red',bd=50)
+label.grid(row=0,column=1)
+start()
+print("done")
+root.mainloop()
